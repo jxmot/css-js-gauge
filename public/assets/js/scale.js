19163cf58eec0d645c6d240c09f794d8108b9561
jxmot <webdev@stupiddog.net>
2023-03-31 19:23:56 -0500

    comments

--- public/assets/js/scale.js
+++ public/assets/js/scale.js
@@ -1,4 +1,6 @@
 /*
+    This gist was used here - 
+        * fpillet/scale.js - https://gist.github.com/fpillet/993002
 */
 function scaleValueInt(value, from, to) {
 	var scale = (to[1] - to[0]) / (from[1] - from[0]);
